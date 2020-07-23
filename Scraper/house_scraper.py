be6218502170826abb56ad7a967cd74389165f49
CanadianHombre <benjoclayton@gmail.com>
2020-07-23 10:48:10 -0400

    Update: house_scraper.py
    There was an update to the housing scraper. This includes functions. The days on site is set to 1.

--- Scraper/house_scraper.py
+++ Scraper/house_scraper.py
@@ -1,47 +1,62 @@
+import datetime as dt
+import urllib.parse as urlparse
+
 import bs4
-import requests
 import pandas as pd
-from collections import OrderedDict
-from datetime import datetime
+import requests
 
-link = 'https://www.viewknoxvillerealestate.com/search/quick?city=Knoxville%2CTN&daysonsite=3&maxprice=520000&minbeds=1&minprice=40000&page=1&perpage=100&soldproperty=0&sortby=latest&type=quick'
-source = requests.get(link).text
-soup = bs4.BeautifulSoup(source, 'lxml')
 
-Price = []
-for house in soup.find_all('div',class_="card--list-header"):
-  houses = house.text
-  Price.append(houses)
-  
-zip_code = [] 
-Address = []
-for house in soup.find_all('p',class_="full-address"):
-  houses = house.text.rsplit(' ',1)
-  Address.append(houses[0])
-  zip_code.append(houses[1])
+def parse_nums(text, cast_to=int):
+    try:
+        return cast_to(''.join(n for n in text if n.isdigit()))
+    except ValueError:
+        return None
 
-Beds = []
-Baths = []
-Area = []
-for house in soup.find_all('div',class_="card--list-stats"):
-  houses = house.text.strip()
-  info = houses.split('\n')
-  Beds.append(info[0].split('B')[0])
-  Baths.append(info[1].split('B')[0])
-  Area.append(info[2])
 
-df = pd.DataFrame(OrderedDict({'Price':Price, 'Area': Area}))
-raw_price = df['Price'].replace({'\$': '', ',': '', 'n/a':'1'}, regex=True)
-raw_area =  df['Area'].replace({',':'','SqFt': '','n/a':'1'}, regex=True)
-raw_price = [int(i) for i in raw_price]
-raw_area = [int(i) for i in raw_area]
-Value = [x/y for x, y in zip(raw_price, raw_area)] #Price per square foot
-Value = [ '%1.f' % elem for elem in Value ] # rounds numbers to one decimal place
-df = pd.DataFrame(OrderedDict({'Zip Code' : zip_code,'Home Address': Address,'Price':raw_price, 'Beds': Beds,
-                               'Baths':Baths, 'Area':raw_area , 'Price/sqft': Value }))
-df['Price/sqft'] = df['Price/sqft'].astype('int')
+def main_scraper(query):
+    url = 'https://www.viewknoxvillerealestate.com/search/quick'
+    r = requests.get(url, params=query)
+    soup = bs4.BeautifulSoup(r.content, 'lxml')
+    all_homes = []
+    for card in soup('div', 'card--list'):
+        beds, baths, sqft = (parse_nums(li.text) for li in card('li'))
+        home = {
+            'address'   : card.find('p', 'full-address').text,
+            'zip_code'  : card.find('p', 'full-address').text.split(' ')[-1],
+            'price'     : parse_nums(card.find('div', 'card--list-header').text),
+            'price_sqft': None,
+            'sqft'      : sqft,
+            'beds'      : beds,
+            'baths'     : baths,
+            'url'       : urlparse.urljoin(url, card.a['href']),
+        }
+        if home['price'] and home['sqft']:
+            home['price_sqft'] = home['price'] // home['sqft']
 
-# Adds the current Date and Time Column for Tracking purposes
-current_date = datetime.date(datetime.now())
-df['Name'] = current_date
-df.to_csv('HousingData.csv',index = False)
+        all_homes.append(home)
+
+    return all_homes
+
+
+def main():
+    query = {
+        'city'        : 'Knoxville,TN',
+        'daysonsite'  : 2,
+        'maxprice'    : 520000,
+        'minbeds'     : 1,
+        'minprice'    : 40000,
+        'page'        : 1,
+        'perpage'     : 10000,
+        'soldproperty': 0,
+        'sortby'      : 'latest',
+        'type'        : 'quick',
+    }
+    data = []
+    data.extend(main_scraper(query))
+    df = pd.DataFrame(data)
+    df['date_scraped'] = dt.date.today()
+    df.to_csv('HousingData.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
